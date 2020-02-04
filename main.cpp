44020f3976fd4df08f670f90e7be412d41dbb2a0
Lu <chris.lu@syccuxas01.pcc.edu>
2020-02-03 17:50:55 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//Practice 01 for Exam
+#include "main.h"
+
+using namespace std;
+
+int main(int argc, char **argv, char **envp)
+{
+//parse some text 
+//write some functions
+//gdb that stuff
+
+
+return 0;
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//system libraries
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cctype>
+#include <fstream>
+
+// constants
